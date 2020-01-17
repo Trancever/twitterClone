4e673f8421ffabf33e0ef02d94f10076c6a6ec49
Trancever <dawidu@onet.pl>
2020-01-17 13:51:22 +0100

    fix: disable animation

--- src/bottomTabs.tsx
+++ src/bottomTabs.tsx
@@ -47,6 +47,7 @@ export const BottomTabs = () => {
           .rgb()
           .string()}
         theme={navigationTheme}
+        sceneAnimationEnabled={false}
       >
         <Tab.Screen
           name="Feed"
